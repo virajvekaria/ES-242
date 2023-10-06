68c20e76a11362a949a300f8593a8b58b6a6a282
virajvekaria <virajv5593@gmail.com>
2023-10-06 19:47:54 +0530

    Adding all helper functions

--- connect4.c
+++ connect4.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
+#include <assert.h>
 
 /* Implement connect 4 on a 5 (columns) x 4 (rows) board. */
 enum
@@ -18,8 +20,18 @@ void init_board(board_t *board)
     {
         for (int col = 0; col < 5; ++col)
         {
-            (*board)[row][col] = 0;
+            (*board)[row][col] = '0';
         }
+    }
+}
+
+player_t other_player(player_t player){
+    switch (player)
+    {
+    case 'R': return 'B';
+    case 'B': return 'R';
+    default:
+        assert(0);
     }
 }
 
@@ -82,13 +94,29 @@ int is_full(board_t board)
     {
         for (int j = 0; j < 5; ++j)
         {
-            if (board[i][j] == 0)
+            if (board[i][j] == '0')
             {
                 return 0;
             }
         }
     }
     return 1;
+}
+
+int score(board_t board, player_t player){
+    assert(has_won(board, player));
+    int score = 10;
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 5; ++j)
+        {
+            if (board[i][j] == player)
+            {
+                score -= 1;
+            }
+        }
+    }
+    return score;
 }
 
 typedef struct
@@ -99,6 +127,90 @@ typedef struct
 
 move_t best_move(board_t board, player_t player)
 {
+    move_t response;
+    move_t candidate;
+    int no_candidate = 0;
+
+    assert(!is_full(board));
+    assert(!has_won(board, 1));
+    assert(!has_won(board, 2));
+
+    for (int row = 0; row < 4; ++row)
+    {
+        for (int col = 0; col < 5; ++col)
+        {
+            if(board[row][col] == '0'){
+                board[row][col] = player;
+                if(has_won(board, player)){
+                    candidate = (move_t) {.col = col,
+                    .score = score(board, player)};
+                    return candidate;
+                }
+            board[row][col] = '0';
+            }
+        }
+        
+    }
+
+    for (int row = 0; row < 4; ++row)
+    {
+        for (int col = 0; col < col; col++)
+        {
+            if(board[row][col] == '0'){
+                board[row][col] = player;
+                if(is_full(board)){
+                    candidate = (move_t){
+                        .col = col,
+                        .score = 0
+                    };
+                return candidate;
+                }
+            
+            response = best_move(board, other_player(player));
+            board[row][col] = '0';
+            if(response.score < 0){
+                candidate = (move_t){
+                    .col = col,
+                    .score = 10 + response.score
+                };
+                return candidate;
+            }
+            else if (response.score = 0){
+                candidate = (move_t){
+                    .col = col,
+                    .score = 0
+                };
+                return candidate;
+            }
+            else{
+                if(no_candidate){
+                candidate = (move_t){
+                    .col = col,
+                    .score = -(response.score)
+                };
+                no_candidate = 0;
+                }
+            }
+            }
+        }
+        
+    }
+    return candidate;
+    
+}
+
+int drop_piece(board_t board, int col, player_t player)
+{
+    assert(board[3][col] == '0' || board[2][col] == '0' || board[1][col] == '0' || board[0][col] == '0');
+    for (int row = 3; row >= 0; --row)
+    {
+        if (board[row][col] == '0')
+        {
+            board[row][col] = player;
+            return row;
+        }
+    }
+    return -1; // Invalid move
 }
 
 void print_board(board_t board)
@@ -107,7 +219,7 @@ void print_board(board_t board)
     {
         for (int col = 0; col < 5; ++col)
         {
-            printf("%2d ", board[row][col]);
+            printf("%c ", board[row][col]);
         }
         printf("\n");
     }
@@ -118,7 +230,41 @@ int main()
 {
     /* Your game play logic. */
     /* The user should have the option to select red or blue player. */
-    board_t trial;
-    init_board(&trial);
+    int move;
+    board_t board;
+    init_board(&board);
+
+    move_t response;
+    player_t current = 'R';
+
+    while (true)
+    {
+        print_board(board);
+        if (current == 'R')
+        {
+            printf("Enter your move: ");
+            scanf("%d", &move);
+            drop_piece(board, move, current);
+        }
+        else
+        {
+            response = best_move(board, current);
+            drop_piece(board, response.col, current);
+        }
+        if (has_won(board, current))
+        {
+            print_board(board);
+            printf("Player %c has won!\n", current);
+            break;
+        }
+        else if (is_full(board))
+        {
+            print_board(board);
+            printf("Draw.\n");
+            break;
+        }
+        current = other_player(current);
+    }
+
     return 0;
 }
